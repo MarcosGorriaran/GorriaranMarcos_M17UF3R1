1052fc79b9c7da2f6dc876331b0d23f2552ffa47
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2025-05-05 17:01:26 +0200

    Made a script to disable fog.

--- Assets/Scripts/AnimatorHandler.cs
+++ Assets/Scripts/AnimatorHandler.cs
@@ -4,9 +4,4 @@ public abstract class AnimatorHandler : MonoBehaviour
 {
     [SerializeField]
     protected Animator _animator;
-
-    protected virtual void Awake()
-    {
-        _animator = GetComponent<Animator>();
-    }
 }
